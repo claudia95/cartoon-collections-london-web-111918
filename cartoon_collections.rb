eab9ecf80758bd293b4ec439f7387fe5320c5226
Claudia Fox <claudiafox95@icloud.com>
2018-11-13 09:03:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  name.capitalize
-  array.collect {|name| name +"!"}
+  element.capitalize
+  array.collect {|element| element +"!"}
 end
 
 def long_planeteer_calls# code an argument here
